70251839665ec4c26b3ac0e0e5edef7e27333471
Ellie <elinor.connors@googlemail.com>
2018-10-02 17:27:06 +0100

    tried to create a list

--- Project/components/QuestList.js
+++ Project/components/QuestList.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, FlatList } from 'react-native';
 import QuestDetails from './QuestDetails'
+import questList from '../mock-data/quest'
 
 class QuestList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      string: 'hi'
+      quests: questList
     };
   }
 
@@ -14,6 +15,13 @@ class QuestList extends Component {
     return (
       <View>
         <Text> This is a list of all the quests</Text>
+        <FlatList
+          data={[
+            { key: 'hello' },
+            { key: 'goodbye' },
+          ]}
+          renderItem={({ item }) => <Text>{item.key}</Text>}
+        />
         <QuestDetails navigation={this.props.navigation} />
       </View>
     );
---
+++ Project/mock-data/quest.js
@@ -0,0 +1,18 @@
+const questList = [
+  {
+    "id": 1,
+    "location": "MOSE",
+    "title": "Science of the Ages",
+    "description": "An intriguing hunt through the science of the industrial revolution and first computers",
+    "suitability": "For 8-10 year olds"
+  },
+  {
+    "id": 2,
+    "location": "MOSE",
+    "title": "The Industrial revolution",
+    "description": "Hunt through one of the most important revolutions of modern times, which changed societty and work in a huge way.",
+    "suitability": "For 8-10 year olds"
+  }
+]
+
+module.exports = questList
